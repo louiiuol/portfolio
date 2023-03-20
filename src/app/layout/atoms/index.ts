58f68acd6fcf1d9823e3f47c2c7d52d2152a3b9c
louiiuol <louis.godlewski@vidmizer.com>
2023-03-20 18:20:17 +0100

    📝 Fixed redundance

--- src/app/layout/atoms/index.ts
+++ src/app/layout/atoms/index.ts
@@ -1,5 +1,5 @@
 /**
- * Atoms components. They are minimal UI elements, made to build bigger components
+ * Minimal UI elements, made to build bigger components
  * like molecules, or to be part of organisms .. Check Atomic design for more information !
  */
 export {IconComponent} from './icon/icon.component';
