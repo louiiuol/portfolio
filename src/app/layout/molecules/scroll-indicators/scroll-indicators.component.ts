6fe89b09a5c1588c3f5056e0d18e463ee81e53b4
louiiuol <louis.godlewski@vidmizer.com>
2023-03-20 21:43:22 +0100

    ✨ Added scroll-indicators

--- src/app/layout/molecules/scroll-indicators/scroll-indicators.component.ts
+++ src/app/layout/molecules/scroll-indicators/scroll-indicators.component.ts
@@ -7,15 +7,35 @@ import {
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 
+/**
+ * Simple components to indicate sections and redirect to their routerLink.
+ * @author louiiuol
+ * @version 1.0.0
+ */
 @Component({
 	selector: 'lou-scroll-indicators',
 	standalone: true,
 	imports: [CommonModule, RouterModule],
-	templateUrl: './scroll-indicators.component.html',
+	template: `<a
+		*ngFor="let link of routes"
+		class="inline-block surface-300 w-1rem h-1rem border-circle"
+		[routerLink]="[link]"
+		[routerLinkActiveOptions]="{exact: true}"
+		routerLinkActive="active"></a>`,
+	styles: [
+		`
+			lou-scroll-indicators a.active {
+				background-color: var(--surface-0) !important;
+				transform: scale(1.2) translateY(1px);
+			}
+		`,
+	],
 	encapsulation: ViewEncapsulation.None,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ScrollIndicatorsComponent {
 	@HostBinding('class') class =
-		'block surface-ground w-full h-3rem md:h-full md:w-3rem';
+		'block w-full h-2rem mb-4 md:mb-0 md:h-full md:w-2rem ml-4 py-5 flex md:flex-column justify-content-center align-items-center gap-3';
+
+	readonly routes = ['/', '/about', '/projects'];
 }
