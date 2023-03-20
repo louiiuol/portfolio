4ac3385b4e61caf27cbda2eed1178d0bfbbdb08d
louiiuol <louis.godlewski@vidmizer.com>
2023-03-20 21:47:04 +0100

    🚧 Init AboutComponent layout

--- src/app/layout/pages/about/about.component.ts
+++ src/app/layout/pages/about/about.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, HostBinding, ViewEncapsulation} from '@angular/core';
+import {TabViewModule} from 'primeng/tabview';
 
+/**
+ * Presentation of myself, my skills and experiences
+ * @author louiiuol
+ * @version 0.0.1
+ */
 @Component({
-  selector: 'lou-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+	standalone: true,
+	imports: [TabViewModule],
+	selector: 'lou-about',
+	templateUrl: './about.component.html',
+	styleUrls: ['./about.component.scss'],
+	encapsulation: ViewEncapsulation.None,
 })
 export class AboutComponent {
-
+	@HostBinding('class') class = 'flex-1 md:h-full';
 }
