f13522bda274940106d38ed03e086d0cdb934824
Louis Godlewski <louis.godlewski@gmail.com>
2025-02-11 18:28:44 +0100

    [Core] Init application layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	imports: [RouterOutlet],
+	template: `<div class="flex bg-primary pt-12">
+		<h1 class="text-2xl font-medium text-center">Welcome to my {{ title }}</h1>
+		<router-outlet></router-outlet>
+	</div>`,
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'portfolio';
+	title = 'portfolio';
 }
