b2f07f672e2620c28fa6fe2d4a0ad7eb98dc0918
Louis Godlewski <louis.godlewski@gmail.com>
2025-02-12 09:25:34 +0100

    [Shared] Renamed IconMaterialComponent

--- src/app/shared/components/atoms/icon/icon.component.ts
+++ src/app/shared/components/atoms/icon/icon.component.ts
@@ -14,7 +14,7 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
  * @author louiiuol
  */
 @Component({
-	selector: 'app-icon',
+	selector: 'app-icon-material',
 	standalone: true,
 	host: {
 		'class': 'material-symbols-outlined inline-block',
