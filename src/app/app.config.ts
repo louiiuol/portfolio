9ad13f5ffe0170cc3966e4f57ef5d1f2428d8345
Louis Godlewski <louis.godlewski@gmail.com>
2025-02-12 09:26:03 +0100

    [DX] Configure angular animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,19 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import type { ApplicationConfig } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
 import {
-	provideClientHydration,
-	withEventReplay,
+  provideClientHydration,
+  withEventReplay,
 } from '@angular/platform-browser';
-import {routes} from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
-		provideZoneChangeDetection({eventCoalescing: true}),
+		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideRouter(routes),
 		provideClientHydration(withEventReplay()),
+		provideAnimationsAsync(),
 	],
 };
