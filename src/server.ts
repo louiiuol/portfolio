c491039057d0e7172894e6ae98f0a05093cfa272
Louis Godlewski <louis.godlewski@gmail.com>
2025-02-13 23:34:18 +0100

    [API] Introduce "/api/hello" route

--- src/server.ts
+++ src/server.ts
@@ -26,27 +26,31 @@ const angularApp = new AngularNodeAppEngine();
  * ```
  */
 
+app.get('/api/hello', (req, res) => {
+	res.json({ message: 'Hello from Express!' });
+});
+
 /**
  * Serve static files from /browser
  */
 app.use(
-  express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: false,
-    redirect: false,
-  }),
+	express.static(browserDistFolder, {
+		maxAge: '1y',
+		index: false,
+		redirect: false,
+	})
 );
 
 /**
  * Handle all other requests by rendering the Angular application.
  */
 app.use('/**', (req, res, next) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
+	angularApp
+		.handle(req)
+		.then(response =>
+			response ? writeResponseToNodeResponse(response, res) : next()
+		)
+		.catch(next);
 });
 
 /**
@@ -54,10 +58,10 @@ app.use('/**', (req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+	const port = process.env['PORT'] || 4000;
+	app.listen(port, () => {
+		console.log(`Node Express server listening on http://localhost:${port}`);
+	});
 }
 
 /**
