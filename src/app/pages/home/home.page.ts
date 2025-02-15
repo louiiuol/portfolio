f0c4aaf64ee2f1046c6dae1976af813729ae171d
Louis Godlewski <louis.godlewski@gmail.com>
2025-02-15 17:39:38 +0100

    [Shared] Introduce TypistComponent

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,13 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IconLogoComponent } from '../../shared/components/atoms/icon/icon-logo.component';
+import { TypistComponent } from '../../shared/components/molecules/typist.component';
 
 @Component({
 	selector: 'app-home-page',
   host: { class: 'page items-center justify-center bg-linear-to-t from-primary-300 to-primary-700'},
 	template: `
-    <app-icon-logo class="w-full max-w-[70%]" variant="expanded" />
+    <app-icon-logo class="w-full max-w-[80%] md:max-w-3xl" variant="expanded" />
+    <app-typist
+      class="text-3xl md:text-4xl lg:text-5xl"
+      [toType]="['Hello World!', 'This is a typing effect.']" />
   `,
-  imports: [IconLogoComponent],
+  imports: [IconLogoComponent, TypistComponent],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage {
---
+++ src/app/shared/components/molecules/typist.component.spec.ts
@@ -0,0 +1,39 @@
+import type { ComponentFixture } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { TypistComponent } from './typist.component';
+
+describe('TypistComponent', () => {
+	let component: TypistComponent;
+	let fixture: ComponentFixture<TypistComponent>;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [TypistComponent],
+		}).compileComponents();
+
+		fixture = TestBed.createComponent(TypistComponent);
+		component = fixture.componentInstance;
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should have default values', () => {
+		expect(component.infinite()).toBe(false);
+		expect(component.typingSpeed()).toBe(50);
+		expect(component.erasingSpeed()).toBe(45);
+		expect(component.newTextDelay()).toBe(1.5);
+		expect(component.startingDelay()).toBe(1.5);
+	});
+
+	it('should handle empty toType array', done => {
+		fixture.componentRef.setInput('toType', []);
+		component.typingState$.subscribe(state => {
+			expect(state.typingValue).toBe('');
+			expect(state.isTyping).toBe(false);
+			expect(state.isDoneTyping).toBe(false);
+			done();
+		});
+	});
+});
