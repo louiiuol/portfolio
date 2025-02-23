816ec9f403172059c70d2daa0372bcfba5cbe493
Louis Godlewski <louis.godlewski@gmail.com>
2025-02-23 11:37:22 +0100

    [DX] Init env variable + fix tailwind intellisense

---
+++ src/environments/contentfull.env.ts
@@ -0,0 +1,7 @@
+export const contenftullEnv = {
+	space: 'pld2vl35l0vx',
+	accessToken: 'f2YRPjhgvuUyw_T-nzy9FnL2nDavixjxl548lcNDuQU',
+	contentTypeIds: {
+		experience: '??',
+	},
+} as const;
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+import { contenftullEnv } from './contentfull.env';
+import type { AppEnvironment } from './type';
+
+export const environment: AppEnvironment = {
+	production: true,
+	contenftull: contenftullEnv,
+};
