2e5b9e4bb89a7a5c5104215603eb70343b7484f8
Louis Godlewski <louis.godlewski@gmail.com>
2025-03-12 23:43:25 +0100

    [Shared] Introduce SearchInputComponent

---
+++ src/app/shared/components/molecules/search-input/search-input.component.ts
@@ -0,0 +1,72 @@
+import {
+	ChangeDetectionStrategy,
+	Component,
+	effect,
+	input,
+	output,
+} from '@angular/core';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { IconMaterialComponent } from '@shared/components/atoms/icon/icon.component';
+import type { nullish } from '@shared/types/nullish.type';
+import { isNotNullish } from '@shared/types/nullish.type';
+import { debounceTime, filter, map, shareReplay, startWith } from 'rxjs';
+
+@Component({
+	selector: 'app-search-input',
+	host: {
+		class:
+			'flex items-center gap-2 outline-2 outline-primary-300 text-primary-800 py-0.5 px-1 rounded-lg',
+	},
+	template: `
+		<app-icon-material name="search" />
+		<div>
+			<input
+				class="appearance-none bg-transparent border-none focus:outline-none"
+				role="searchbox"
+				type="search"
+				[formControl]="searchControl"
+				[placeholder]="placeholder()"
+				[style.maxWidth]="maxWidth()"
+				[style.minWidth]="minWidth()"
+				[style.width]="inputWidth()" />
+			@if (hint()) {
+				<p class="text-xs text-gray-500 mt-2">{{ hint() }}</p>
+			}
+		</div>
+	`,
+	imports: [ReactiveFormsModule, IconMaterialComponent],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SearchInputComponent {
+	readonly value = input<string | nullish>();
+	readonly placeholder = input('Rechercher par mot clé');
+	readonly hint = input<string>();
+	readonly searchChanged = output<string>();
+	readonly minWidth = input('21ch');
+	readonly maxWidth = input('30ch');
+
+	protected readonly searchControl = new FormControl('');
+	protected readonly setSearchControlValue = effect(() => {
+		const searchValue = this.value();
+		if (searchValue) {
+			this.searchControl.setValue(searchValue);
+		}
+	});
+
+	protected readonly searchTermSubscription = this.searchControl.valueChanges
+		.pipe(
+			debounceTime(400),
+			startWith(this.value()),
+			takeUntilDestroyed(),
+			shareReplay(1),
+			filter(isNotNullish)
+		)
+		.subscribe(searchValue => this.searchChanged.emit(searchValue));
+
+	protected readonly inputWidth = toSignal(
+		this.searchControl.valueChanges.pipe(
+			map(value => `${(value?.length ?? 0) - 5}ch`)
+		)
+	);
+}
