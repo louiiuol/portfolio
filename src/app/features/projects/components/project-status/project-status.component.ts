7efd0cfd0dac4d807dc2197c95b6f343124f55e6
Louis Godlewski <louis.godlewski@gmail.com>
2025-05-18 10:12:05 +0200

    remove dead code

--- src/app/features/projects/components/project-status/project-status.component.ts
+++ src/app/features/projects/components/project-status/project-status.component.ts
@@ -28,34 +28,4 @@ export class ProjectStatusComponent {
 	protected readonly statusInfo = computed(() =>
 		getProjectStatusInfo(this.status())
 	);
-
-	protected readonly statusColor = computed(() => {
-		switch (this.status()) {
-			case 'idea':
-				return '#e6df5c';
-			case 'draft':
-				return 'text-blue-500';
-			case 'in_progress':
-				return 'text-green-500';
-			case 'done':
-				return 'text-gray-500';
-			default:
-				return 'text-red-500';
-		}
-	});
-
-	protected readonly statusIcon = computed(() => {
-		switch (this.status()) {
-			case 'idea':
-				return '💡';
-			case 'draft':
-				return '📝';
-			case 'in_progress':
-				return '🔄';
-			case 'done':
-				return '✅';
-			default:
-				return '❓';
-		}
-	});
 }
